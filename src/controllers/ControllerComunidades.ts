1cb0f4d44b324e858ec1a639171bde9f06f27a58
arthurns1 <js.arthurns90@gmail.com>
2025-11-07 21:41:51 -0300

    Ajeitando controllers

--- src/controllers/ControllerComunidades.ts
+++ src/controllers/ControllerComunidades.ts
@@ -15,7 +15,7 @@ export class ControllerComunidades {
             const params = [req.body.nome_comunidade];
 
             await pool.query(
-                "INSERT INTO comunidades (id_mensagem, nome_comunidade) VALUES (DEFAULT, $1)",
+                "INSERT INTO comunidades (id_comunidade, nome_comunidade) VALUES (DEFAULT, $1)",
                 params,
             );
 
@@ -49,7 +49,7 @@ export class ControllerComunidades {
 
     static async get_comunidade_by_id(req: Request, res: Response) {
         try {
-            const params = [req.params.id_comunidade];
+            const params = [req.params.id];
 
             const results = await pool.query(
                 "SELECT * FROM comunidades WHERE id_comunidade = $1;",
@@ -70,10 +70,10 @@ export class ControllerComunidades {
 
     static async update_comunidade_by_id(req: Request, res: Response) {
         try {
-            const params = [req.params.id_comunidade, req.body.nome_comunidade];
+            const params = [req.params.id, req.body.nome_comunidade];
 
             const results = await pool.query(
-                "UPDATE comunidades WHERE id_comunidade = $1 SET nome_comunidade = $2;",
+                "UPDATE comunidades SET nome_comunidade = $2 WHERE id_comunidade = $1 ;",
                 params,
             );
 
@@ -91,7 +91,7 @@ export class ControllerComunidades {
 
     static async delete_comunidade_by_id(req: Request, res: Response) {
         try {
-            const params = [req.params.id_comunidade];
+            const params = [req.params.id];
 
             const results = await pool.query(
                 "DELETE FROM comunidades WHERE id_comunidade = $1;",
