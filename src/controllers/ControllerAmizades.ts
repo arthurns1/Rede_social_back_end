a9916e5239a613970ca2a3a8ff3851c3b8c7bfd7
arthurns1 <js.arthurns90@gmail.com>
2025-11-07 22:01:25 -0300

    Ajeitando controller de amizade

--- src/controllers/ControllerAmizades.ts
+++ src/controllers/ControllerAmizades.ts
@@ -35,7 +35,8 @@ export class ControllerAmizades {
 
     static async get_all_user_amizades(req: Request, res: Response) {
         try {
-            const params = [req.body.login_usuario];
+            const params = [req.params.login];
+
             const results = await pool.query(
                 "SELECT * FROM usuarios u JOIN amizades a ON ((a.login_usuario = $1 AND a.login_amigo = u.login) OR (a.login_amigo = $1 AND a.login_usuario = u.login)) WHERE a.status = 'aceita';",
                 params,
@@ -53,12 +54,36 @@ export class ControllerAmizades {
         }
     }
 
+    static async get_all_pedidos_amizade(req: Request, res: Response) {
+        try {
+            const params = [req.params.login_usuario];
+            const results = await pool.query(
+                "SELECT * FROM amizades WHERE login_usuario = $1;",
+                params,
+            );
+
+            res.status(201).json({
+                success_message: "",
+                results: results.rows,
+            });
+        } catch (err) {
+            res.status(500).json({
+                error_message: "Houve um erro interno ao retornar amizades!",
+                error: err,
+            });
+        }
+    }
+
     static async confirm_amizade(req: Request, res: Response) {
         try {
-            const params = [req.body.login_amigo, req.body.login_usuario];
+            const params = [
+                req.body.login_amigo,
+                req.body.login_usuario,
+                "aceita",
+            ];
 
             const results = await pool.query(
-                "UPDATE FROM amizades WHERE login_amigo = $1 AND login_usuario = $2 SET status = 'aceita';",
+                "UPDATE amizades SET status = $3 WHERE login_amigo = $1 AND login_usuario = $2;",
                 params,
             );
 
--- src/routes/amizade.ts
+++ src/routes/amizade.ts
@@ -3,9 +3,16 @@ import { ControllerAmizades } from "../controllers/ControllerAmizades";
 
 const amizade = Router();
 
-amizade.get("/get_all_user_amizades", ControllerAmizades.get_all_user_amizades);
+amizade.get(
+    "/get_all_user_amizades/:login",
+    ControllerAmizades.get_all_user_amizades,
+);
+amizade.get(
+    "/get_all_pedidos_amizade/:login",
+    ControllerAmizades.get_all_pedidos_amizade,
+);
 amizade.post("/ask_amizade", ControllerAmizades.ask_amizades);
 amizade.put("/confirm_amizade", ControllerAmizades.confirm_amizade);
-amizade.get("/delete_amizade", ControllerAmizades.delete_amizade_by_id);
+amizade.delete("/delete", ControllerAmizades.delete_amizade_by_id);
 
 export { amizade };
